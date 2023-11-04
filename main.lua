8c74ad60175c7a650059910e8e68f8cb0471c966
jaiden <panic24@proton.me>
2023-11-04 17:21:02 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
 function love.load()
-    --insert initializing code here
+    love.window.setMode(1152, 768) --window dimensions
+    love.graphics.setBackgroundColor(114/255, 159/255, 207/255) --sets background to sky blue
 end
 
 function love.draw()
-    love.graphics.print("test message", 400, 300)
+    love.graphics.setColor(1, 1, 1) --default white
+    love.graphics.rectangle('fill', 768, 0, 640, 768)
+    love.graphics.setColor(175/255, 1, 175/255) --green grass
+    love.graphics.rectangle('fill', 0, 512, 768, 256)
 end
