08aff8630dc53e4ef4726b2fad5daf7496ffe080
ikixu <147871408+ikixu@users.noreply.github.com>
2023-11-04 20:40:06 -0500

    main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 function love.load()
     love.window.setMode(1152, 768) --window dimensions
-    love.graphics.setBackgroundColor(114/255, 159/255, 207/255) --sets background to sky blue
+    background = love.graphics.newImage('graphics/background.png') --background
+    cloud1 = love.graphics.newImage('graphics/cloud2.png')
+    frame = love.graphics.newImage('graphics/frame.png') --sapling
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1) --default white
     love.graphics.rectangle('fill', 768, 0, 640, 768)
-    love.graphics.setColor(175/255, 1, 175/255) --green grass
-    love.graphics.rectangle('fill', 0, 512, 768, 256)
+    love.graphics.draw(background) --background
+    love.graphics.draw(cloud1, 0, 0, 0, 1, 1)
+    love.graphics.draw(frame, 125, 150) --frame
+    love.graphics.setColor(0, 0, 0) --black bar
+    love.graphics.rectangle('fill', 768, 0, 10, 768)
 end
